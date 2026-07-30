9390ae695c4b7159340ddae87822d0f77a2dd070
kgforsure <kgforsure@www.kgforsure.horse>
2026-07-30 14:54:23 +0800

    b

--- main_agent.py
+++ main_agent.py
@@ -1,56 +1,93 @@
 '''
-用來跑最終地圖的模型
+用來訓練走路模型的agent 
+TODO
 '''
 from stable_baselines3 import PPO
 from stable_baselines3.common.env_checker import check_env
-from main_game_env import stand
+from main_game_env import stand, walk
 import time
-if __name__ == "__main__":
-    # 1. 建立環境
-    env = stand(render_mode="no") # 如果想加速訓練不看畫面，可改為 "direct"
-    
-    # 2. 檢查環境格式是否符合 Gymnasium 標準
+import os
+import argparse
+
+def train_m(env_name):
+    os.makedirs("./models", exist_ok=True)
+    match env_name:
+        case 'stand':
+            env = stand(render_mode = "no")
+        case 'walk':
+            env = walk(render_mode = "no")
+        # case 'grab':
+        #     env = grab(render_mode = "no")
+        # case 'bi':
+        #     env = bi(render_mode = "no")
+        case _:
+            print('task not found')
+            return
     check_env(env, warn=True)
     print("環境格式檢驗合格！")
-
-    # 3. 初始化 PPO 模型
-    # MlpPolicy 代表使用多層感知機 (全連接網路) 處理向量輸入
-    model = PPO("MlpPolicy", env, verbose=1, learning_rate=3e-4, n_steps=2048)
-
-    # 4. 開始訓練 (你可以先用少量的 timesteps 測試，例如 10000)
-    print("開始訓練 PPO 模型...")
-    model.learn(total_timesteps=5000)
-
-    # 5. 儲存模型
-    model.save("ppo_door_out_agent")
-    print("模型已儲存！")
-    env.close()
-
-
-    #--------- 
-    env = stand(render_mode="human")
     obs, info = env.reset()
 
-    
-    # 建立 agent
-    agent = PPO("MlpPolicy", env, verbose=1, learning_rate=3e-4, n_steps=2048)
-    agent.load("./ppo_door_out_agent.zip")
-    
-    # 跑幾次動畫看看成果
-    for episode in range(20):
-        state, _ = env.reset()
-        done = False
-        total_reward = 0
-        #it will hardly done, because too hard to reach goal
-        while not done: 
-            # 測試時直接用 select_action，此時 agent 已經很聰明了
-            action, _= agent.predict(obs, deterministic=True)
-            next_state, reward, terminated, truncated, _ = env.step(action)
-            done = terminated or truncated
-            state = next_state
-            total_reward += reward
-            
-        print(f"Test Episode {episode+1}, Reward: {total_reward:.2f}")
-        time.sleep(0.5)
-        print("AAAAAAAAAAAAAAAAAAAAAAAAAA")
+    model = PPO("MlpPolicy", env, verbose=1, learning_rate=3e-4, n_steps=2048,device="cpu")
+    step_record = 25_000
+    ite = 0
+    while 1:
+        ite+=1
+        model.learn(total_timesteps=step_record,reset_num_timesteps=False)
+        file_name = f"./models/ppo_{env_name}_agent_{ite*step_record}"
+        model.save(file_name)
+    # test_m(file_name,ep)
     env.close()
+
+
+def test_m(env_name,file_name):
+
+    match env_name:
+        case 'stand':
+            env = stand(render_mode = "human")
+        case 'walk':
+            env = walk(render_mode = "human")
+        # case 'grab':
+        #     env = grab(render_mode = "human")
+        # case 'bi':
+        #     env = bi(render_mode = "human")
+        case _:
+            print('task not found')
+            return
+    check_env(env, warn=True)
+    print("環境格式檢驗合格！")
+    obs, info = env.reset()
+    agent = PPO.load(f"{file_name}.zip", env=env,device="cpu")
+    
+    done = False
+    total_reward = 0
+    while not done: 
+        action, _= agent.predict(obs, deterministic=True)
+        next_obs, reward, terminated, truncated, _ = env.step(action)
+        done = terminated or truncated
+        obs = next_obs
+        total_reward += reward
+
+    env.close()
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Train or test model.')
+    parser.add_argument('gymenv', help='Gymnasium environment i.e. Humanoid-v4',nargs='?',default="Pusher-v5")
+    parser.add_argument('-t', '--train',nargs="?", const="1", default=None,)
+    parser.add_argument('-s', '--test', metavar='path_to_model')
+    args = parser.parse_args()
+
+    env_name = args.gymenv
+    train = args.train
+    test = args.test
+    file_name = args.test
+
+
+
+
+    if train:
+        train_m(env_name)
+    if test:
+        test_m(env_name, file_name)
+
+    
+
+
