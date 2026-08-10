ba3c0b07618d1e136437a40a39cdf8c7c922ae8d
kgforsure <kgforsure@www.kgforsure.horse>
2026-08-10 15:51:45 +0800

    env_all2.py

--- car/env_all2.py
+++ car/env_all2.py
@@ -1,6 +1,4 @@
 '''
-此為中間版本，車子會到每一個點，然後執行抓取任務
-
 等手臂訓練完畢後，就要來整合全部功能
 場景：目標物、讀卡機、門
 初始化：手臂收起來、車子生成在(0,0)、卡片生在5*5的隨機範圍，讀卡機生成在牆壁上、門在固定位置、四周有牆壁
@@ -20,7 +18,7 @@ import math
 config ={
     "gravity":-9.8,
     "startPos":[0,0,0.2],
-    "target_pos":[[5,0,0],[0,5,0],[-3,-3,0],[0,0,0]],
+    "target_pos":[[0,5,0],[5,0,0],[-3,-3,0]], #卡片, 讀卡機, 終點
     "simu_time":10000000000000,
     "force":85,
     "velo":10,
@@ -29,9 +27,11 @@ config ={
     "danger_distance":0.25,
     "detect_d":10,
     "turn_var":0.5,
-    "max_arm_steps":1000
+    "max_arm_steps":1000,
+    "door_timer":0,
+    "trigger_distance":1,
+    "robot_state": "Navigating"
 }
-model = PPO.load("./car/models/car_grap_observation_v2")
 
 
 def detect(config):
@@ -140,14 +140,38 @@ mugs = []
 for i,j in enumerate(config["target_pos"]):
     mugs.append(p.loadURDF("urdf/mug.urdf",j, startOrientation))
 
-arm_step_counter = 0
+
+
+reader_col = p.createCollisionShape(p.GEOM_BOX, halfExtents=[0.2, 0.2, 0.2])
+reader_vis = p.createVisualShape(p.GEOM_BOX, halfExtents=[0.2, 0.2, 0.2], rgbaColor=[0.2, 0.2, 0.8, 1]) # 藍色
+readerId = p.createMultiBody(baseMass=0, baseCollisionShapeIndex=reader_col, baseVisualShapeIndex=reader_vis, basePosition=[5,0,1])
 
 card_col = p.createCollisionShape(p.GEOM_BOX, halfExtents=[0.05, 0.05, 0.05])
 card_vis = p.createVisualShape(p.GEOM_BOX, halfExtents=[0.05, 0.05, 0.05], rgbaColor=[0.8, 0.2, 0.2, 1]) # 紅色
 cardId = p.createMultiBody(baseMass=0.1, baseCollisionShapeIndex=card_col, baseVisualShapeIndex=card_vis, basePosition=[0,5,0.8])
 
+door_link_col = p.createCollisionShape(p.GEOM_BOX, halfExtents=[0.05, 0.5, 1.0])
+door_link_vis = p.createVisualShape(p.GEOM_BOX, halfExtents=[0.05, 0.5, 1.0], rgbaColor=[0.5, 0.5, 0.5, 1]) # 灰色
+doorId = p.createMultiBody(
+    baseMass=0,                        # 基座固定
+    baseCollisionShapeIndex=-1,        # 基座沒有形狀
+    baseVisualShapeIndex=-1,           # 基座沒有外觀
+    basePosition=[-3, -3, 1],        # 整體的起始位置
+    
+    linkMasses=[10.0],
+    linkCollisionShapeIndices=[door_link_col],
+    linkVisualShapeIndices=[door_link_vis],
+    linkPositions=[[0, 0, 0]],                  # 相對基座的位置
+    linkOrientations=[[0, 0, 0, 1]],            # 相對基座的姿態
+    linkInertialFramePositions=[[0, 0, 0]],     # 慣性中心位置
+    linkInertialFrameOrientations=[[0, 0, 0, 1]],# 慣性中心姿態
+    linkParentIndices=[0],                      # 父節點索引 (0 代表連接到基座)
+    linkJointTypes=[p.JOINT_PRISMATIC],         # 關鍵：滑軌關節
+    linkJointAxis=[[0, 0, 1]]                     # 沿著 Y 軸左右移動
+)
+
 #迴圈
-robot_state = "Arm_Grasp" #"Arm_Grasp", "Arm_bi"
+config["robot_state"] = "Navigating" #"Arm_Grasp", "Arm_bi"
 p.resetBasePositionAndOrientation(car, config["startPos"], startOrientation)
 check = 0
 for i in range (config["simu_time"]):
@@ -157,30 +181,35 @@ for i in range (config["simu_time"]):
         time.sleep(2)
         break
     #模式
-    if robot_state == "Navigating":
+    if config["robot_state"] == "Navigating":
         #障礙物偵測
         result = detect(config)
         # 目標前進
         if result[2]*config["detect_d"]>config["danger_distance"]:
             reached = move_to(config["target_pos_id"], config)
             if reached:
-                config["target_pos_id"] += 1
+                
                 print(f"Reached target {config['target_pos_id']}! Stopping car to start arm grasping.")
                 # 煞車停下車子
                 for wheel in [2, 3, 4, 5]:
                     p.setJointMotorControl2(car, wheel, controlMode=p.VELOCITY_CONTROL, targetVelocity=0, force=config["force"])
                 # 切換狀態到手臂抓取
-                robot_state = "Arm_Grasp"   #TODO
-                # robot_state = "Arm_bi"   #TODO
-                time.sleep(3)
+                if config["target_pos_id"] == 0:
+                    config["robot_state"] = "Arm_Grasp"
+                if config["target_pos_id"] == 1:
+                    config["robot_state"] = "Arm_bi"
+                config["target_pos_id"] += 1
                 arm_step_counter = 0
         else:
             for wheel in [2, 3, 4, 5]:
                 p.setJointMotorControl2(car, wheel, controlMode=p.VELOCITY_CONTROL, targetVelocity=0, force=config["force"])
 
-    if robot_state == "Arm_Grasp":
-        ########################################################################
-        card_pos, _ = p.getBasePositionAndOrientation(cardId)   #TODO
+    if config["robot_state"] != "Navigating":
+        if config["robot_state"] == "Arm_Grasp":
+            model = PPO.load("./car/models/car_grap_observation_v2")
+        if config["robot_state"] == "Arm_bi":
+            model = PPO.load("./car/models/car_grap_observation_v2")  #TODO 這邊要用bi卡的模型
+        card_pos, _ = p.getBasePositionAndOrientation(cardId)   
         grap_pos = p.getLinkState(arm,11)[0]
         joint_position = []
         joint_velocity = []
@@ -199,7 +228,6 @@ for i in range (config["simu_time"]):
             relative_pos,
             card_pos
         ]).astype(np.float32)
-        ########################################################################
         action, _states = model.predict(obs, deterministic=True)
         action_joinID_maxV = [
             [0,   0,   2.175, -0.36],
@@ -220,14 +248,13 @@ for i in range (config["simu_time"]):
                 targetVelocity=target_vel,
                 force=50
             )
-        print(arm_step_counter)
         arm_step_counter += 1
 
         # 4. 判斷手臂是否完成抓取，或超時
         # 這裡可以用「超過固定步數」或「夾爪成功夾到物件」來作為結束條件
         if arm_step_counter >= config["max_arm_steps"]:
             print(f"Arm finished grasping for target {config['target_pos_id']}. Resuming navigation.")
-            robot_state = "Navigating"  # 切回車子導航去下一個點
+            config["robot_state"] = "Navigating"  # 切回車子導航去下一個點
             #arm go back stable pos
             for a,j,v,stable_pos in action_joinID_maxV[:-1]:
                 target_pos = stable_pos # 放大係數可依訓練速需求調整
@@ -238,7 +265,20 @@ for i in range (config["simu_time"]):
                     targetPosition=target_pos,
                     force=50
                 )
-        pass
+
+    #reader and card touch sensor
+    closest_points = p.getClosestPoints(cardId, readerId, 1.0)
+    if closest_points:
+        # closest_points[0][8] 代表兩個物體之間的最短距離 (Distance)
+        current_distance = closest_points[0][8]
+        if current_distance < config["trigger_distance"]:
+            config["door_timer"] = 20 * 240  #20秒
+    if config["door_timer"] != 0:
+        config["door_timer"] -= 1
+        p.setJointMotorControl2(bodyUniqueId=doorId, jointIndex=0, controlMode=p.POSITION_CONTROL, targetPosition=5.0, force=500)
+    else:
+        p.setJointMotorControl2(bodyUniqueId=doorId, jointIndex=0, controlMode=p.POSITION_CONTROL, targetPosition=0.0, force=500)
+
     time.sleep(1./240.)
 
 p.disconnect()
